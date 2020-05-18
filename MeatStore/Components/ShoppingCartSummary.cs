c205dbe36eb744051ac29f7df3f9e12a6e6ac9fb
Harold Lockward <HLockward12@gmail.com>
2020-05-18 18:30:24 +0200

    Update ShoppingCartSummary.cs

--- MeatStore/Components/ShoppingCartSummary.cs
+++ MeatStore/Components/ShoppingCartSummary.cs
@@ -10,6 +10,7 @@ namespace MeatStore.Components
         private readonly ShoppingCart _shoppingCart;
         public ShoppingCartSummary(ShoppingCart shoppingCart)
         {
+            _shoppingCart = shoppingCart;
         }
 
         public IViewComponentResult Invoke()
